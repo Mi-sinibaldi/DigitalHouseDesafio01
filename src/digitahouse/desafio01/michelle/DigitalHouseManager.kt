79594cc3bf786a9944a61e9f4b4064db050ee642
mi-sinibaldi <mia_araujo92@hotmail.com>
2020-09-26 23:24:06 -0300

    implementacao da classe Matricula e DigitalHouseManager

---
+++ src/digitahouse/desafio01/michelle/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package digitahouse.desafio01.michelle
+
+data class DigitalHouseManager (
+    var alunos: List<Aluno>,
+    var professores: List<Professor>,
+    var cursos: List<Curso>,
+    var matriculas: List<Matricula>
+)
---
+++ src/digitahouse/desafio01/michelle/Matricula.kt
@@ -0,0 +1,9 @@
+package digitahouse.desafio01.michelle
+
+import java.util.*
+
+data class Matricula (
+    var aluno: Aluno,
+    var curso: Curso,
+    var dtMatricula: Date
+)
